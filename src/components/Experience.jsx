21392cf882dfc494dc2e7e2a4164d81481c73eec
Gulzey <Gxlz4418@gmail.com>
2024-07-28 21:33:47 +0100

    xyz

---
+++ src/components/Experience.jsx
@@ -0,0 +1,23 @@
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Book } from "./Book";
+export const Experience = () => {
+  return (
+    <>
+    <Book />
+      <OrbitControls />
+      <Environment preset="studio"></Environment>
+      <directionalLight
+        position={[2, 5, 2]}
+        intensity={2.5}
+        castShadow
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+        shadow-bias={-0.0001}
+      />
+      <mesh position-y={-1.5} rotation-x={-Math.PI / 2} receiveShadow>
+        <planeGeometry args={[100, 100]} />
+        <shadowMaterial transparent opacity={0.2} />
+      </mesh>
+    </>
+  );
+};
